8a2597f16a89fc4dce9abb76e6b2d1b2e75e7d58
otherJL0 <jonathanglopez@gmail.com>
2021-11-15 13:12:23 -0500

    Adding tree-sitter parser

--- lispy/main.py
+++ lispy/main.py
@@ -1,17 +1,23 @@
 import sys
 
 from prompt_toolkit import PromptSession
+from tree_sitter import Language, Parser
 
 
 def evaluate(line: str):
     if line in ["exit", "exit()", "quit", "quit()"]:
         print("Goodbye!")
         sys.exit(0)
-    print(line)
 
 
 def main():
+    Language.build_library("data/elisp.so", ["tree-sitter-elisp"])
+    ELISP_LANGUAGE = Language("data/elisp.so", "elisp")
+    parser = Parser()
+    parser.set_language(ELISP_LANGUAGE)
     session: PromptSession[str] = PromptSession()
     while True:
         line: str = session.prompt("prompt-kit: lispy> ")
         evaluate(line)
+        tree = parser.parse(bytes(line, "utf8"))
+        print(tree.root_node.sexp())
